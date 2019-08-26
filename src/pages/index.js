fe911773123e971d084937691fcf4301243feb6e
gsim2345 <gb@gabriellasimon.me>
2019-08-26 11:39:57 +0200

    Added new pages with basic texts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
-import React from "react"
+import React from 'react';
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+            <h2>I'm a frontend developer living in a lovely northern europe city.</h2>
+        </div>
+    )
+}
+
+export default IndexPage;
