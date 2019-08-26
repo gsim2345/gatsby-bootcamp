58a7edca87d2ae6205a4efb5d830e8f777579260
gsim2345 <gb@gabriellasimon.me>
2019-08-26 15:18:54 +0200

    Styled header

--- src/components/header.js
+++ src/components/header.js
@@ -5,21 +5,23 @@ import headerStyles from './header.module.scss';
 
 const Header = () => {
     return (
-        <header>
-            <h1>Gabriella Simon</h1>
+        <header className={headerStyles.header}>
+            <h1>
+                <Link to='/' className={headerStyles.title}>Gabriella Simon</Link>
+            </h1>
             <nav>
-                <ul>
+                <ul className={headerStyles.navList}>
                     <li>
-                        <Link className={headerStyles.link} to='/'>Home</Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/'>Home</Link>
                     </li>
                     <li>
-                        <Link to='/blog'>Blog</Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/blog'>Blog</Link>
                     </li>
                     <li>
-                        <Link to='/about'>About Me</Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/about'>About Me</Link>
                     </li>
                     <li>
-                        <Link to='/contact'>Contact</Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/contact'>Contact</Link>
                     </li>
                 </ul>
             </nav>
