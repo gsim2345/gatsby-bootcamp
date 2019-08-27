8b0357e5be46a8983d500f078f723381741cdfd7
gsim2345 <gb@gabriellasimon.me>
2019-08-27 09:58:23 +0200

    generated post list from markdown data through graphhQL queries

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,40 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 import Layout from '../components/layout';
 
 
+
 const BlogPage = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            allMarkdownRemark {
+                edges {
+                  node {
+                    frontmatter {
+                      title
+                      date
+                    }
+                  }
+                }
+              }
+        }
+    `);
+
+    
     return (
         <div>
             <Layout>
                 <h1>Blog</h1>
-                <p>Posts will show up later on.</p>
+                <ol>
+                    {data.allMarkdownRemark.edges.map((edge, i) => {
+                        return (
+                            <li key={i}>
+                                <h2>{edge.node.frontmatter.title}</h2>
+                                <p>{edge.node.frontmatter.date}</p>
+                            </li>
+                        )
+                    })}
+                </ol>
             </Layout>
             
         </div>
