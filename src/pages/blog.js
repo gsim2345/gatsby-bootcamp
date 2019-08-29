2279c0d9fc12bed1d19da6d6b8733a99aaf8f08b
gsim2345 <gb@gabriellasimon.me>
2019-08-29 20:54:35 +0200

    Added styles to blog page and footer

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, graphql, useStaticQuery } from 'gatsby';
 import Layout from '../components/layout';
+import blogStyles from './blog.module.scss';
 
 
 
@@ -28,10 +29,10 @@ const BlogPage = () => {
         <div>
             <Layout>
                 <h1>Blog</h1>
-                <ol>
+                <ol className={blogStyles.posts}>
                     {data.allMarkdownRemark.edges.map((edge, i) => {
                         return (
-                            <li key={i}>
+                            <li key={i} className={blogStyles.post}>
                                 <Link to={`/blog/${edge.node.fields.slug}`}>
                                     <h2>{edge.node.frontmatter.title}</h2>
                                     <p>{edge.node.frontmatter.date}</p>
