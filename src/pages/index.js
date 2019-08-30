b2233dd0e7674c74ed09824198bf97b66ff33df1
gsim2345 <gb@gabriellasimon.me>
2019-08-30 14:29:20 +0200

    Added 404 page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => {
     return (
         <Layout>
             <h1>Hello</h1>
-            <h2>I'm a frontend developer living in a lovely northern europe city.</h2>
+            <h2>I'm a frontend developer living in a lovely northern europian city.</h2>
             <p>Need a developer? <Link to='/contact'>Contact Me!</Link></p>
         </Layout>
 
