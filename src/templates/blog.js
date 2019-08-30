2f57bda920be463b86016e59e4171b10a10a9307
gsim2345 <gb@gabriellasimon.me>
2019-08-30 12:42:15 +0200

    Added image from Contentful

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import Layout from '../components/layout';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 // not using staticquery in template file, instead define it separately, and export it. Because no way to access the context which containes our slug if we use the useStaticquery
 // gatsby grabs this graphql query, the $slug will come from the variable that we set up to create the page. 
@@ -30,16 +31,30 @@ export const query = graphql`
         contentfulBlogPost (slug : {eq: $slug }) {
             title
             publishedDate (formatString: "MMMM Do, YYYY")
-
+            body {
+                json
+            }
         }
     }
 `
 
 const Blog = (props) => {
+
+    const options = {
+        // customize how that node is rendered
+        renderNode:  {
+            "embedded-asset-block": (node) => {
+                const alt = node.data.target.fields.title['en-US'];
+                const url = node.data.target.fields.file['en-US'].url;
+                return <img alt={alt} src={url}/>
+            }
+        }
+    }
     return (
         <Layout>
             <h1>{props.data.contentfulBlogPost.title}</h1>
             <p>{props.data.contentfulBlogPost.publishedDate}</p>
+            {documentToReactComponents(props.data.contentfulBlogPost.body.json, options)}
         </Layout>
     );
 }
